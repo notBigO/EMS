1e26a52c60b421089797826f04369e9b63688ef8
Varun <2003varunp@gmail.com>
2025-02-11 21:18:34 +0530

    emp entity and repository setup

---
+++ src/main/java/com/ms/ems/entities/Employee.java
@@ -0,0 +1,40 @@
+package com.ms.ems.entities;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @NotBlank(message = "Role is mandatory")
+    private String role;
+
+    @NotBlank(message = "Department is mandatory")
+    private String department;
+
+    @ElementCollection
+    private Set<String> skills = new HashSet<>();
+
+    @ElementCollection
+    private Set<String> assignedProjects = new HashSet<>();
+}
---
+++ src/main/java/com/ms/ems/repositories/EmployeeRepository.java
@@ -0,0 +1,10 @@
+package com.ms.ems.repositories;
+
+import com.ms.ems.entities.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    Optional<Employee> findByEmail(String email);
+}
